40ed530f3bd6c3b362617523ec751e332c683e4c
Vamsikataru333 <80826556+Vamsikataru333@users.noreply.github.com>
2021-05-11 08:22:00 +0530

    jQuery

---
+++ 10-05-2021/jQuery HandsOn/js/hands-on-1.js
@@ -0,0 +1,7 @@
+$(() => {
+    $('#selected-plays > li > ul > li').addClass('special');
+
+    $('td:nth-child(3)').addClass("year");
+    
+    $('td:contains(Tragedy)').eq(0).parent().addClass('special');
+});
